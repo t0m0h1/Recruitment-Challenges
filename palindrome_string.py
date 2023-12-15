1da8034602bc139fe955ab8fe96676be91f61b0e
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2023-12-15 22:29:29 +0000

    palindrome_string

---
+++ palindrome_string.py
@@ -0,0 +1,18 @@
+# input: a string
+s = 'a man, a plan, a canal: panama'
+
+def preprocess(s: str) -> str:
+
+    # Preprocess a string to remove all non-alphanumeric characters and convert to lowercase.
+    return "".join(filter(str.isalnum, s)).lower()
+
+def palindrome(s: str) -> bool:
+
+    # Check if a string is a palindrome.
+    return s == s[::-1]
+
+
+if __name__ == '__main__':
+    print('\n')
+    print('Palindrome String: ')
+    print(palindrome(preprocess(s)))

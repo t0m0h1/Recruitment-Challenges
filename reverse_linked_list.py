233f22e3b15a56582de5cd44257005ec4beefc25
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2023-12-17 15:36:21 +0000

    changes

---
+++ reverse_linked_list.py
@@ -0,0 +1,10 @@
+# challenge: Reverse a linked list
+
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def reverse_linked_list(head: LinkedList) -> LinkedList:
+    pass

ce84ec10aea7db56ad53cbd27da0e5e5997ae723
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2023-12-17 15:20:17 +0000

    changes

--- binary_search.py
+++ binary_search.py
@@ -1,11 +1,15 @@
 # Iterative Binary Search
 # it works by repeatedly dividing the search interval in half
+# Time Complexity: O(log n)
+
+# Iterative Binary Search
+# It works by repeatedly dividing the search interval in half.
 
 def binary_search(arr, left, right, x: int) -> int:
     while left <= right:
         mid = left + (right - left) // 2
 
-        # check if x is mid
+        # Check if x is at mid
         if arr[mid] == x:
             return mid
         
@@ -20,9 +24,9 @@ def binary_search(arr, left, right, x: int) -> int:
 # Test array
 arr = [1, 2, 3, 4, 5, 6, 7, 8]
 
-result = binary_search(arr, 0, len(arr)-1, 8)
-
 if __name__ == '__main__':
+    result = binary_search(arr, 0, len(arr)-1, 8)
+
     if result != -1:
         print(f'Element is present at index {result}')
     else:

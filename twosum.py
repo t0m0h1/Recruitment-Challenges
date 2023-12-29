de7ef3f6f47a752691fd274ca23b6e5df31c87d7
t0m0h1 <thomashathawayhogg@gmail.com>
2023-12-29 18:09:07 +0000

    changes

--- twosum.py
+++ twosum.py
@@ -1,3 +1,5 @@
+# Description: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
 def twosum(arr, target):
     l, r = 0, len(arr) -1
 
@@ -10,11 +12,12 @@ def twosum(arr, target):
         else:
             return [l, r]
 
-
-
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 target = 8
 
 if __name__ == '__main__':
     # call twosum() with nums and target as arguments.
-    print(twosum(nums, target))
+    result = twosum(nums, target)
+    print("Result:", result)
+
+    

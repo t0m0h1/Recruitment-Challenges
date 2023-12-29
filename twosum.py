953f667222d0522717c0848301bc2e6feb840da4
t0m0h1 <thomashathawayhogg@gmail.com>
2023-12-29 18:06:08 +0000

    2nd attempt

--- twosum.py
+++ twosum.py
@@ -1,12 +1,20 @@
 def twosum(arr, target):
-    for i in arr:
-        for j in arr:
-            if i + j == target:
-                return arr[i], arr[j]
+    l, r = 0, len(arr) -1
+
+    while l < r:
+        current_sum = arr[l]+ arr[r]
+        if current_sum > target:
+            r -= 1
+        elif current_sum < target:
+            l += 1
+        else:
+            return [l, r]
+
 
 
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+target = 8
 
 if __name__ == '__main__':
-    # call twosum with nums and the target number
-    print(twosum(nums, 9))
+    # call twosum() with nums and target as arguments.
+    print(twosum(nums, target))

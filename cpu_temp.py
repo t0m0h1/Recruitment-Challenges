18ab6ce7f5fe9b76a0fb05ceadfc8d6d20ffc180
t0m0h1 <thomashathawayhogg@gmail.com>
2024-01-08 18:57:45 +0000

    changes

--- cpu_temp.py
+++ cpu_temp.py
@@ -1,8 +1,19 @@
 import psutil
 
-def get_cpu_temp():
-    temp = psutil.sensors_temperatures()
-    return temp['coretemp'][0].current
+def get_cpu_temperature():
+    try:
+        temperature = psutil.sensors_temperatures()['coretemp'][0].current
+        return temperature
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
 
-if __name__ == '__main__':
-    print(get_cpu_temp())
+if __name__ == "__main__":
+    cpu_temperature = get_cpu_temperature()
+
+    if cpu_temperature is not None:
+        print(f"CPU Temperature: {cpu_temperature}°C")
+    else:
+        print("Failed to retrieve CPU temperature.")
+
+

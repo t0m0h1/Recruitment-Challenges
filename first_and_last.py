a08c805d4931c7a8e5f7bb152be2240f16111af5
t0m0h1 <thomashathawayhogg@gmail.com>
2024-01-17 17:36:40 +0000

    changes

--- first_and_last.py
+++ first_and_last.py
@@ -4,4 +4,15 @@
 
 
 def first_and_last(arr, n):
-    pass
+    for i in range(len(arr)):
+        if arr[i] == n:
+            start = i
+            while i+1 < len(arr) and arr[i+1] == n:
+                i += 1
+            return [start, i]
+    return [-1, -1]
+
+if __name__ == '__main__':
+    arr = [1, 3, 5, 5, 5, 5, 67, 123, 125]
+    n = 5
+    print(first_and_last(arr, n))

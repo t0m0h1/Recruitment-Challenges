b6f7d6e3f72a36cf25e55836e66c8e793e5c3bb4
t0m0h1 <thomashathawayhogg@gmail.com>
2024-02-04 21:23:30 +0000

    solution

--- count_occurrences.py
+++ count_occurrences.py
@@ -7,5 +7,10 @@ def element_count(arr):
             count[el] = 1
     return count
 
-array = [1, 3, 4, 4, 4, 5]
+array = [1, 3, 4, 4, 4, 5, 5]
 print(element_count(array))
+
+
+'''Inside the loop, it checks if the current element (el) is already a key in the count dictionary using the in operator.
+If it is, it means that the element has already been encountered before.
+so the code increments its count by 1 using the += operator.'''
